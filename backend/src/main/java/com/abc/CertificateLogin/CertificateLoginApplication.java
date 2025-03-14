832550444cc0974a108fdf9f6b90d1bfca70b4f6
AhmetBahaCepni <ahmetbahacepni5335@gmail.com>
2025-03-14 16:14:47 +0300

    init

---
+++ backend/src/main/java/com/abc/CertificateLogin/CertificateLoginApplication.java
@@ -0,0 +1,13 @@
+package com.abc.CertificateLogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertificateLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CertificateLoginApplication.class, args);
+	}
+
+}
